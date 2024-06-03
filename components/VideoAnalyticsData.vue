ba463c95dda331db0ae7141cc753485ad188ade3
Olanetsoft <heedris2olubisi@gmail.com>
2024-06-03 23:26:49 +0100

    fix: update unique views calculation

--- components/VideoAnalyticsData.vue
+++ components/VideoAnalyticsData.vue
@@ -10,8 +10,10 @@
   <div v-else class="flex flex-col items-center w-full">
     <div class="grid grid-cols-1 gap-6 w-full">
       <div class="bg-white rounded-lg p-4 shadow-md text-center">
-        <h3 class="text-lg font-semibold text-gray-700 mb-2">Total Views</h3>
-        <p class="text-3xl font-bold text-gray-900">{{ totalViews }}</p>
+        <h3 class="text-lg font-semibold text-gray-700 mb-2">
+          Total Unique Views
+        </h3>
+        <p class="text-3xl font-bold text-gray-900">{{ uniqueViews }}</p>
       </div>
     </div>
   </div>
@@ -35,5 +37,17 @@ onMounted(async () => {
   }
 });
 
-const totalViews = computed(() => analyticsData.value?.data.length || 0);
+const uniqueViews = computed(() => {
+  if (!analyticsData.value?.data) return 0;
+
+  const uniqueViewSet = new Set();
+
+  analyticsData.value.data.forEach((view) => {
+    // Create a unique identifier for each view
+    const uniqueKey = `${view.viewer_application_name}-${view.view_ended_at}`;
+    uniqueViewSet.add(uniqueKey);
+  });
+
+  return uniqueViewSet.size;
+});
 </script>
